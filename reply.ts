af3163d1b588fe549a643f0c15709e0cd0ece151
kkjonnykk <28173360+kkjonnykk@users.noreply.github.com>
2023-04-16 19:12:24 +0100

    refactored and added exports

--- reply.ts
+++ reply.ts
@@ -8,19 +8,6 @@ import * as dotenv from 'dotenv';
 import process from 'node:process';
 dotenv.config();
 
-const reply = process.argv.indexOf('--reply-to') !== -1
-
-const parentURL = process.argv[process.argv.indexOf('--reply-to') + 1];
-
-const textcontentflag = process.argv.indexOf('--text-content') !== -1;
-
-const textcontent = process.argv[process.argv.indexOf('--text-content') + 1];
-
-if (!textcontentflag || !textcontent || !reply || !parentURL) {
-  console.error("ERROR: Please provide a text content for the bleet with the --text-content flag, and a parent post with the --reply-to flag.")
-  process.exit(1)
-}
-
 const agent = new BskyAgent({
   service: 'https://bsky.social',
 });
@@ -30,41 +17,22 @@ await agent.login({
   password: process.env.BSKY_PASSWORD!,
 });
 
+const getParentPost = async (parentURL: string) => {
 
-// Get username and post ID from parent URL
+  const parentDomain = parentURL.split("/")[4]
+  const parentPostID = parentURL.split("/")[6]
 
-const parentDomain = parentURL.split("/")[4]
-const parentPostID = parentURL.split("/")[6]
+  const parentFeed = await agent.getAuthorFeed({actor: parentDomain})
+  const parentPost = parentFeed.data.feed.find(post => post.post.uri.split("/")[4] === parentPostID)
 
-// Get parent post
-
-const parentFeed = await agent.getAuthorFeed({actor: parentDomain})
-const parentPost = parentFeed.data.feed.find(post => post.post.uri.split("/")[4] === parentPostID)
-
-if (!parentPost) {
-  console.error("ERROR: Parent post not found.")
-  process.exit(1)
+  if (parentPost) {
+    return parentPost
+  } else {
+    return undefined
+  }
 }
 
-// Get parent post's uri and cid
-
-const parentURI = parentPost.post.uri
-const parentCID = parentPost.post.cid
-const parentHandle = JSON.parse(JSON.stringify(parentPost.post.author)).handle
-
-// Get paren't post's root uri and cid
-
-const parentData = JSON.parse(JSON.stringify(parentPost.reply))
-
-const parentRootURI = parentData.root.uri
-const parentRootCID = parentData.root.cid
-
-console.log(`\nReplying to ${parentHandle} with the following content:\n----------`)
-console.log(textcontent + "\n----------")
-
-// Define sendReply function (async because it uses await and I wanna call it within a readline question)
-
-async function sendReply() {
+const postReply = async (agent, textcontent: string, parentRootURI: string, parentRootCID: string, parentURI: string, parentCID: string) => {
   await agent.post({
     text: textcontent,
     reply: {
@@ -80,17 +48,63 @@ async function sendReply() {
   })
 }
 
-// Ask user to confirm
+// Very inefficient way to find the post, but it works with just the URL. I'm sure there's a better way to do this.
+export const reply = async (agent, textcontent: string, parentURL: string) => {
+
+  const parentPost = await getParentPost(parentURL)
+  
+  if (!parentPost) {
+    console.error("ERROR: Could not find parent post.")
+    process.exit(1)
+  }
+
+  const parentURI = parentPost.post.uri
+  const parentCID = parentPost.post.cid
+  
+  let parentRootURI = parentURI;
+  let parentRootCID = parentCID;
+  if (parentPost.reply) {
+    const parentData = JSON.parse(JSON.stringify(parentPost.reply));
+    parentRootURI = parentData.root.uri;
+    parentRootCID = parentData.root.cid;
+  }
+
+  await postReply(agent, textcontent, parentRootURI, parentRootCID, parentURI, parentCID)
+}
+
+
+const replyflag = process.argv.indexOf('--reply-to') !== -1
+
+const parentURL = process.argv[process.argv.indexOf('--reply-to') + 1];
+
+const textcontentflag = process.argv.indexOf('--text-content') !== -1;
+
+const textcontent = process.argv[process.argv.indexOf('--text-content') + 1];
+
+if (!textcontentflag || !textcontent || !replyflag || !parentURL) {
+  console.error("ERROR: Please provide a text content for the bleet with the --text-content flag, and a parent post with the --reply-to flag.")
+  process.exit(1)
+}
+
+const parentPost = await getParentPost(parentURL)
+
+if (!parentPost) {
+  console.error("ERROR: Could not find parent post.")
+  process.exit(1)
+}
+
+const parentHandle = JSON.parse(JSON.stringify(parentPost.post.author)).handle
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
+console.log(`Replying to ${parentHandle}'s post at ${parentURL} with the text content:\n----------\n${textcontent}\n----------`)
 rl.question(`Are you sure you want to post this reply to ${parentHandle}'s post? (Type SEND to confirm) `, (answer) => {
   if (answer === "SEND") {
     console.log("Posting reply...")
-    sendReply()
+    reply(agent, textcontent, parentURL)
     console.log("Reply hopefully posted successfully.")
     rl.close()
   } else {
